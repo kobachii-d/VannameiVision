c486d5c19ebd7eadebfb3301fed50bd7640189b2
Kobchai Duangrattanalert <kduangrattanalert@gmail.com>
2023-08-09 09:55:01 +0700

    Update __init__.py

--- vannameivision/__init__.py
+++ vannameivision/__init__.py
@@ -1,8 +1,1 @@
-import warnings
-
-# Suppress the TensorFlow Addons warning
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=UserWarning, module="tensorflow_addons")
-
-# Now, your regular imports for this package
-from .utils import build, read_preprocess, make_prediction, get_image_paths
+from .utils import get_image_paths, build, read_preprocess, make_prediction
